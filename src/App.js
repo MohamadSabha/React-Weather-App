2283d34f7904c78aec90fe193904c3674e865fa0
Mohamad Sabha <mhd.sabha@hotmail.com>
2025-06-22 02:04:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./newstyle.css";
   crossorigin="anonymous"
 />;
 function App() {
-  const API_KEY = "your_api_key_here";
+  const API_KEY = "YOUR_API_KEY";
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const [weatherData, setWeatherData] = useState(null);
